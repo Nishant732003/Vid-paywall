2fd2c8b7595934da78321762c4f0c5f17725d7f9
nishant <mahtonishantraj@gmail.com>
2024-10-09 02:04:00 +0530

    razorpay integration

--- src/app/api/[[...route]]/payment.ts
+++ src/app/api/[[...route]]/payment.ts
@@ -2,12 +2,13 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { verifyAuth } from "@hono/auth-js";
 import { z } from "zod";
+import { razorpay } from "@/lib/razorpay";
 
 const app = new Hono()
     .post("/create-order",
         verifyAuth(),
         zValidator("json", z.object({
-            planId:z.string()
+            planId: z.string()
         })),
         async (c) => {
             const session = c.get("authUser");
@@ -18,12 +19,14 @@ const app = new Hono()
             let options = {
                 amount: 999,
                 currency: "INR",
-                receipt:"order_rcptid_11"
+                receipt: "order_rcptid_11"
             }
-
-           
+            const order = await (razorpay.orders.create(options));
+            if (!order) {
+                return c.json({ error: "order creation failed" }, 500);
+            }
         }
-    )
+    );
 
 export default app;
 
